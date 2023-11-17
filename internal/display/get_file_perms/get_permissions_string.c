1bbef408b375586ed129ecb332e79150a6c116b7
Colin Heurteaux <colin.heurteaux@epitech.eu>
2023-11-17 16:38:37 +0100

    Fixed perms string ?

--- internal/display/get_file_perms/get_permissions_string.c
+++ internal/display/get_file_perms/get_permissions_string.c
@@ -28,7 +28,7 @@ char *get_permisions_string(struct stat *fs_item_stat)
     get_owner_perms(fs_item_stat, result);
     get_group_perms(fs_item_stat, result);
     get_general_perms(fs_item_stat, result);
-    if (S_ISDIR(fs_item_stat->st_mode) && (fs_item_stat->st_mode & S_ISVTX))
+    if (fs_item_stat->st_mode & S_ISVTX)
         result[9] = 'T';
     return result;
 }
